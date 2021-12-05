0d113f1eec4ffb404d786f9cb83ec8f291d66e69
wrigh <wrightde@gmail.com>
2021-12-05 15:25:26 -0800

    Day one donesies

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -11,5 +11,31 @@ def DayOne(text):
         depth = x
     return depthChanges
 
+def DayTwo(text):
+    x = 0
+    y = 0
+    firstSet = 0
+    secondSet = 0
+    sets = []
+    depthChanges = 0
+    for i in text:
+        sets.append(text[x:x + 3])
+        x += 1
+    print(sets)
+    for i in range(len(sets)-1):
+        for i in sets[y]:
+            firstSet += i
+        for i in sets[y+1]:
+            secondSet += i
+        print (firstSet)
+        print (secondSet)
+        if firstSet < secondSet:
+            depthChanges += 1
+        print(depthChanges)
+        y += 1
+        firstSet = 0
+        secondSet = 0
+    return depthChanges
 
-print(DayOne(sweeps))
+
+print(DayTwo(sweeps))
