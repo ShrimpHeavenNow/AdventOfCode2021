dc885a8273f932b953626b602ebd80c87107b2af
wrigh <wrightde@gmail.com>
2021-12-07 23:06:24 -0800

    Day 4 started

---
+++ Day 4/day4.py
@@ -0,0 +1,15 @@
+with open('bingo.txt') as f:
+    bingo = [line.rstrip() for line in f]
+print(bingo)
+
+# Separate ball draws and bingo boards
+
+# Create grid from bingo boards
+
+# Figure out a way to mark a spot
+
+# Iterate through ball draws, marking boards
+
+# check if board has won
+
+# Do math on board
