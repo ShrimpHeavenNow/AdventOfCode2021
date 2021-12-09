07d80bb9469d23be46ce7d513f0be5aa70df2178
wrigh <wrightde@gmail.com>
2021-12-08 21:31:17 -0800

    Day 5 getting there.

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,4 +1,4 @@
-with open('vents.txt') as f:
+with open('ventstest.txt') as f:
     vents = [line.strip() for line in f]
 import numpy as np
 
@@ -6,15 +6,9 @@ lines = []
 
 
 def Parse(line):
-    # print(line)
     start, end = line.split(" -> ")
     x1, y1 = int(start.split(',')[0]), int(start.split(',')[1])
     x2, y2 = int(end.split(',')[0]), int(end.split(',')[1])
-    # print(line)
-    # print(x1)
-    # print(y1)
-    # print(x2)
-    # print(y2)
     return x1, y1, x2, y2
 
 
@@ -29,24 +23,27 @@ def PartOne(lines):
             print("This bitch is vertical")  # +1 these coordinates in the array.
             MarkArray(map, x, "vert")
     overlaps = map[map > 1]
+    print(map)
     print('The amount of overlaps is :' + str(len(overlaps)))
 
 
 def MarkArray(array, line, dir):  # I have all the pieces but don't know how to put them together.
     if dir == "horz":
-        steps = int(abs(line[2] - line[3]))
+        steps = int(abs(line[1] - line[3]))
         print(line)
         print("steps")
         print(steps)
-        for range
-            print("a butt")
-    if dir == "vert":
+        for x in range(steps):
+            array[line[0], [x]] += 1  # This part is wrong
+
+    if dir == "vert":  # And now the hard part...
         steps = abs(line[0] - line[2])
         print(line)
         print("steps")
         print(steps)
+        for x in range(steps):
+            array[x ,[line[1]]] +=1  #This part is wrong
 
-print(vents)
 
 for x in vents:
     lines.append(Parse(x))
