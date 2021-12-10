76811258563ae95e78e4fa96b1da7f56e902d5b2
wrigh <wrightde@gmail.com>
2021-12-09 19:44:39 -0800

    Day 5 Done!

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,8 +1,9 @@
-with open('ventstest.txt') as f:
+with open('vents.txt') as f:
     vents = [line.strip() for line in f]
 import numpy as np
+from skimage.draw import line  # Thank god I found this.
 
-lines = []
+lines = []  # I don't see other people doing this which makes me think it's wrong.
 
 
 def Parse(line):
@@ -12,37 +13,19 @@ def Parse(line):
     return x1, y1, x2, y2
 
 
-def PartOne(lines):
-    map = np.zeros(shape=(np.max(lines) + 1, np.max(lines) + 1))
-    # print(map)
+def PartOne(lines):  # Who needs a part 2 anyway?
+    map1 = np.zeros(shape=(np.max(lines) + 1, np.max(lines) + 1))  # This used to be its own function
+    map2 = np.zeros(shape=(np.max(lines) + 1, np.max(lines) + 1))  # Oh, how much I've grown, what a fool I was.
     for x in lines:
-        if x[0] == x[2]:  # Mark horizontal line.
-            print("Horizontal baby")  # +1 these coordinates in the array
-            MarkArray(map, x, "horz")
-        if x[1] == x[3]:  # Mark vertical line. This could probably combine with above.
-            print("This bitch is vertical")  # +1 these coordinates in the array.
-            MarkArray(map, x, "vert")
-    overlaps = map[map > 1]
-    print(map)
-    print('The amount of overlaps is :' + str(len(overlaps)))
-
-
-def MarkArray(array, line, dir):  # I have all the pieces but don't know how to put them together.
-    if dir == "horz":
-        steps = int(abs(line[1] - line[3]))
-        print(line)
-        print("steps")
-        print(steps)
-        for x in range(steps):
-            array[line[0], [x]] += 1  # This part is wrong
-
-    if dir == "vert":  # And now the hard part...
-        steps = abs(line[0] - line[2])
-        print(line)
-        print("steps")
-        print(steps)
-        for x in range(steps):
-            array[x ,[line[1]]] +=1  #This part is wrong
+        x1, y1, x2, y2 = x
+        rr, cc = line(x1, y1, x2, y2)  # Originally I was painstakingly iterating through coordinates.
+        if x1 == x2 or y1 == y2:
+            map1[rr, cc] += 1
+        map2[rr, cc] += 1
+    overlaps1 = map1[map1 > 1]
+    overlaps2 = map2[map2 > 1]
+    print("Part one overlaps: " + str(len(overlaps1)))
+    print("Part two overlaps: " + str(len(overlaps2)))
 
 
 for x in vents:
