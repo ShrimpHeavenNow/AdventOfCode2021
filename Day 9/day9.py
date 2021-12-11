e6ea2a24d417191f42531e0e0c72f6a30aeeea13
wrigh <wrightde@gmail.com>
2021-12-10 21:38:44 -0800

    Day 9 Part one done. Part 2 is... on its way, I think. How are 9s sneaking in!?

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -1,4 +1,4 @@
-with open('floor.txt') as f:
+with open('floortest.txt') as f:
     floor = [line.strip() for line in f]
 
 
@@ -31,10 +31,59 @@ def PartOne(floor):
         ypos += 1
     print(sum(lowpoints))  # Having a list and summing was better for debugging. Why am I justifying myself?
 
-def FindNines(floor,xpos,ypos,areacoords):
-    if floor[ypos-1][xpos-1]
+
+def FindNines(floor, xpos, ypos, areacoords):
+    xmax = len(floor[0])
+    ymax = len(floor)
+    print(ypos)
+    print(xpos)
+    print(floor[ypos][xpos])
+    print(areacoords)
+    if floor[ypos - 1][xpos - 1] != 9:  # Find way to not go beyond 0 or max
+        if (ypos - 1, xpos - 1) not in areacoords.keys():
+            areacoords[(ypos - 1, xpos - 1)] = False
+    if floor[ypos + 1][xpos - 1] != 9:
+        if (ypos + 1, xpos - 1) not in areacoords.keys():
+            areacoords[(ypos - 1, xpos - 1)] = False
+    if floor[ypos - 1][xpos + 1] != 9:
+        if (ypos - 1, xpos + 1) not in areacoords.keys():
+            areacoords[(ypos - 1, xpos + 1)] = False
+    if floor[ypos + 1][xpos + 1] != 9:
+        if (ypos + 1, xpos + 1) not in areacoords.keys():
+            areacoords[(ypos + 1, xpos + 1)] = False
+    areacoords[(ypos, xpos)] = True
+    for x in areacoords:  # do FindNines to all the falses.
+        if floor[x[0]][x[1]] == 9:
+            print("WHAT THE HELL")
+        elif areacoords[x] == False:
+            FindNines(floor, x[0], x[1], areacoords)
+
+    return areacoords
+
+        # make a dict of coords with value of checked or not. add unchecked adjacent non-9s to dict. check all non checked.
+        # Once fully checked, return the full list
+
 
 def PartTwo(floor):
+
+    ypos = 1
+    areasizes = []
+    for z in floor[1:-1]:
+        areacoords = {}
+        xpos = 1
+        for d in z:
+            areacoords[(ypos, xpos)] = False
+            areacoords = FindNines(floor, xpos, ypos, areacoords)
+            xpos += 1
+            areasizes.append(len(areacoords))
+            for i in areacoords.keys():
+                y = i[0]
+                x = i[1]
+                floor[y][x] = 9  # This is either smart or really dumb.
+        ypos += 1
+    print(sum(areasizes))
+
+
     """
     So we need to find an area surrounded by nines.
     find the amount of numbers in that area
