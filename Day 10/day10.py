5f8e81cec4d9ca7da06b94c291f7621c661d6dbb
wrigh <wrightde@gmail.com>
2021-12-11 21:38:56 -0800

    day 10 done. that was oddly simple.

--- Day 10/day10.py
+++ Day 10/day10.py
@@ -14,58 +14,56 @@ def PartOne(lines):
             if y in closes:
                 if opens.index(inside[-1:][0]) != closes.index(y):
                     corruptions.append(y)
-                    print(x,"is corrupted")
                     break
                 else:
                     inside.pop()
 
-    print(corruptions)
     pointValues = {")": 3, "]": 57, "}": 1197, ">": 25137}
     score = 0
     for x in corruptions:
         score += pointValues[x]
-    print(score)
+    return score
 
 
-PartOne(lines)
+def PartTwo(lines):
+    opens = ('(', '[', '<', '{')
+    closes = (')', ']', '>', '}')
+    toRemove = []
+    for x in lines:  # Remove Corrupted.
+        inside = []
+        for y in x:
+            if y in opens:
+                inside.append(y)
+            if y in closes:
+                if opens.index(inside[-1:][0]) != closes.index(y):
+                    toRemove.append(x)
+                    break
+                else:
+                    inside.pop()
 
-opens = ('(', '[', '<', '{')
-closes = (')', ']', '>', '}')
-inside = []
-toremove = []
-for x in lines:  # Remove Corrupted.
-    inside = []
-    for y in x:
-        if y in opens:
-            inside.append(y)
-        if y in closes:
-            if opens.index(inside[-1:][0]) != closes.index(y):
-                toremove.append(x)
-                print(x,"is corrupted")
-                break
+    for x in toRemove:  # There's a better way of doing this I'm sure.
+        lines.remove(x)
+
+    pointValues = {"(": 1, "[": 2, "{": 3, "<": 4}
+    scores = []
+    for x in lines:  # A smart person could do this in one iteration, I bet. Anyway, here's scoring.
+        score = 0
+        inside = []
+        for y in x:
+            if y in opens:
+                inside.append(y)
             else:
                 inside.pop()
+        inside.reverse()
+        for y in inside:
+            score = (score * 5)
+            score += pointValues[y]
+        scores.append(score)
 
-for x in toremove:  # There's a better way of doing this I'm sure.
-    lines.remove(x)
+    scores.sort()
+    mid = len(scores) // 2  # I bet there's a function for this.
+    return scores[mid]
 
-pointValues = {"(": 1, "[": 2, "{": 3, "<": 4}
-scores = []
-for x in lines:  # A smart person could do this in one iteration, I bet. Anyway, here's scoring.
-    score = 0
-    inside = []
-    for y in x:
-        if y in opens:
-            inside.append(y)
-        else:
-            inside.pop()
-    inside.reverse()
-    for y in inside:
-        score = (score * 5)
-        score += pointValues[y]
-    scores.append(score)
 
-scores.sort()
-mid = len(scores)//2
-print(scores)
-print(scores[mid])
+print("Part One ", PartOne(lines))
+print("Part Two: ", PartTwo(lines))
