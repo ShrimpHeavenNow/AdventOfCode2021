c951f43befb9c5b7e93acdfd2150115015ec6dbf
wrigh <wrightde@gmail.com>
2021-12-11 21:28:00 -0800

    day 10 done. that was oddly simple.

--- Day 10/day10.py
+++ Day 10/day10.py
@@ -5,7 +5,6 @@ with open('syntax.txt') as f:
 def PartOne(lines):
     opens = ('(', '[', '<', '{')
     closes = (')', ']', '>', '}')
-    inside = []
     corruptions = []
     for x in lines:
         inside = []
@@ -15,6 +14,7 @@ def PartOne(lines):
             if y in closes:
                 if opens.index(inside[-1:][0]) != closes.index(y):
                     corruptions.append(y)
+                    print(x,"is corrupted")
                     break
                 else:
                     inside.pop()
@@ -32,24 +32,40 @@ PartOne(lines)
 opens = ('(', '[', '<', '{')
 closes = (')', ']', '>', '}')
 inside = []
-corruptions = []
-print(len(lines))
-for x in lines:
+toremove = []
+for x in lines:  # Remove Corrupted.
     inside = []
     for y in x:
         if y in opens:
             inside.append(y)
         if y in closes:
             if opens.index(inside[-1:][0]) != closes.index(y):
-                corruptions.append(y)
-                lines.remove(x)
+                toremove.append(x)
+                print(x,"is corrupted")
                 break
             else:
                 inside.pop()
 
+for x in toremove:  # There's a better way of doing this I'm sure.
+    lines.remove(x)
 
-pointValues = {")": 1, "]": 2, "}": 3, ">": 4}
-score = 0
-for x in corruptions:
-    score += pointValues[x]
-print(score)
+pointValues = {"(": 1, "[": 2, "{": 3, "<": 4}
+scores = []
+for x in lines:  # A smart person could do this in one iteration, I bet. Anyway, here's scoring.
+    score = 0
+    inside = []
+    for y in x:
+        if y in opens:
+            inside.append(y)
+        else:
+            inside.pop()
+    inside.reverse()
+    for y in inside:
+        score = (score * 5)
+        score += pointValues[y]
+    scores.append(score)
+
+scores.sort()
+mid = len(scores)//2
+print(scores)
+print(scores[mid])
